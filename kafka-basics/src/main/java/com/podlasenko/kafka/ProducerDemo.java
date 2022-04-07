0a8a195fb3497b627180301831d67b561a342d44
Yevhen Podlasenko <jeka113381@gmail.com>
2022-04-07 22:05:34 +0300

    [learn] add Producer and put first record to local Kafka cluster

--- kafka-basics/src/main/java/com/podlasenko/kafka/ProducerDemo.java
+++ kafka-basics/src/main/java/com/podlasenko/kafka/ProducerDemo.java
@@ -1,7 +1,43 @@
 package com.podlasenko.kafka;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
 public class ProducerDemo {
+    private final static Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
     public static void main(String[] args) {
-        System.out.println("test");
+        log.info("----- main start -----");
+
+        // create Producer Properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // create a producer record
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>(
+                "demo_java",
+                "Hello from java!");
+
+        // send the data - asynchronous
+        producer.send(producerRecord);
+
+        // flush data - synchronous
+        producer.flush();
+
+        // flush and close
+        producer.close();
+
+        log.info("----- main end -----");
     }
 }

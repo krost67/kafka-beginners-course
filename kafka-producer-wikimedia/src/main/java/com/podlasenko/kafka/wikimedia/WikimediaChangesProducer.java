3518b0de81ccbdb095b99db2053519c80106ccc3
Yevhen Podlasenko <jeka113381@gmail.com>
2022-04-17 14:06:42 +0300

    [learn] add safe Kafka Producer config example (for old Kafka versions <= 2.8)

--- kafka-producer-wikimedia/src/main/java/com/podlasenko/kafka/wikimedia/WikimediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/com/podlasenko/kafka/wikimedia/WikimediaChangesProducer.java
@@ -5,8 +5,10 @@ import com.launchdarkly.eventsource.EventSource;
 import com.podlasenko.kafka.utils.KafkaUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 
 import java.net.URI;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import static com.podlasenko.kafka.utils.KafkaUtils.WIKIMEDIA_TOPIC_NAME;
@@ -19,9 +21,14 @@ import static com.podlasenko.kafka.utils.KafkaUtils.WIKIMEDIA_URL;
 public class WikimediaChangesProducer {
 
     public static void main(String[] args) throws InterruptedException {
+        Properties producerConfig = KafkaUtils.getKafkaProducerProperties();
+        // set safe producer config (only for Kafka version <= 2.8 )
+        producerConfig.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        producerConfig.put(ProducerConfig.ACKS_CONFIG, "all");
+        producerConfig.put(ProducerConfig.RETRIES_CONFIG, String.valueOf(Integer.MAX_VALUE));
+
         // create the producer
-        KafkaProducer<String, String> producer = new KafkaProducer<>(
-                KafkaUtils.getKafkaProducerProperties());
+        KafkaProducer<String, String> producer = new KafkaProducer<>(producerConfig);
 
         EventHandler eventHandler = new WikimediaChangeHandler(producer, WIKIMEDIA_TOPIC_NAME);
         EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(WIKIMEDIA_URL));

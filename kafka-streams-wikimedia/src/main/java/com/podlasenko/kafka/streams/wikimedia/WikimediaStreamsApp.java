357e805009a8687bf92679e61416521f3e824c91
Yevhen Podlasenko <jeka113381@gmail.com>
2022-07-10 22:29:24 +0300

    [learn] add Kafka streams demo

---
+++ kafka-streams-wikimedia/src/main/java/com/podlasenko/kafka/streams/wikimedia/WikimediaStreamsApp.java
@@ -0,0 +1,53 @@
+package com.podlasenko.kafka.streams.wikimedia;
+
+import com.podlasenko.kafka.streams.wikimedia.processor.BotCountStreamBuilder;
+import com.podlasenko.kafka.streams.wikimedia.processor.EventCountTimeseriesBuilder;
+import com.podlasenko.kafka.streams.wikimedia.processor.WebsiteCountStreamBuilder;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.KStream;
+
+import java.util.Properties;
+
+
+/**
+ * Demo for Kafka streams
+ */
+
+@Slf4j
+public class WikimediaStreamsApp {
+
+    private static final Properties properties;
+    private static final String INPUT_TOPIC = "wikimedia.recentchange";
+
+    static {
+        properties = new Properties();
+        properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "wikimedia-stats-application");
+        properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+    }
+
+    public static void main(String[] args) {
+        StreamsBuilder builder = new StreamsBuilder();
+        KStream<String, String> changeJsonStream = builder.stream(INPUT_TOPIC);
+
+        BotCountStreamBuilder botCountStreamBuilder = new BotCountStreamBuilder(changeJsonStream);
+        botCountStreamBuilder.setup();
+
+        WebsiteCountStreamBuilder websiteCountStreamBuilder = new WebsiteCountStreamBuilder(changeJsonStream);
+        websiteCountStreamBuilder.setup();
+
+        EventCountTimeseriesBuilder eventCountTimeseriesBuilder = new EventCountTimeseriesBuilder(changeJsonStream);
+        eventCountTimeseriesBuilder.setup();
+
+        final Topology appTopology = builder.build();
+        log.info("Topology: " + appTopology.describe());
+        KafkaStreams kafkaStreams = new KafkaStreams(appTopology, properties);
+        kafkaStreams.start();
+    }
+}
